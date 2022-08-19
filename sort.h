c7c5d3d491b8e844f7600dba3510837145afbb64
Onotu Umoru <onotu128@gmail.com>
2022-08-19 14:58:11 +0100

    new commit

--- sort.h
+++ sort.h
@@ -5,10 +5,12 @@
 #include <stdlib.h>
 
 /*Comparison direction macros for bitonic sort*/
+
 #define UP 0
 #define DOWN 1
 
 /*enum bool - Enumeration of boolean values*/
+
 typedef enum bool
 {
     false = 0,
@@ -42,7 +44,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-
 
 #endif
